cc5f0c7504c14e71fd74e1d3326c5b23c230a486
jrabcs <jrabcs@bhphoto.com>
2016-06-06 18:13:55 -0400

    Modified build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,47 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass 		= require('gulp-sass');
+
+var vendorFiles = [ 'bower_components/angular/angular.min.js' ];
 
 // Static server
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "./app/"
+            baseDir: "./dist/"
         }
     });
 });
 
-gulp.task('default', ['browser-sync']);
+gulp.task('styles', function () {
+	gulp.src('src/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('dist/assets/css/'));
+});
+
+gulp.task('templates', function () {
+
+	gulp.src('src/templates/**/*.html')
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('javascript', function () {
+
+	gulp.src('src/javascript/**/*.js')
+		.pipe(gulp.dest('dist/assets/js/'));
+});
+
+gulp.task('bower', function () {
+
+	gulp.src(vendorFiles)
+		.pipe(gulp.dest('dist/assets/js/vendor/'));
+});
+
+gulp.task('watch', function () {
+
+	gulp.watch('src/sass/**/*', ['styles']);
+	gulp.watch('src/templates/**/*', ['templates']);
+	gulp.watch('src/javascript/**/*', ['javascript']);
+});
+
+gulp.task('default', ['browser-sync', 'bower', 'watch']);
---
+++ src/javascript/app.js
@@ -0,0 +1,4 @@
+angular.module('weatherApp', [])
+	.controller('weatherAppController', function ($scope) {
+		$scope.description = "Hello World!";
+	});
